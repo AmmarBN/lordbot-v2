53081404ce1e4f268efb1aa1c43dcf5cdd712ee5
Lord_Ammar <83591869+AmmarBN@users.noreply.github.com>
2022-01-26 15:06:03 +0000

    Add files via upload

---
+++ plugins/wallq.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch')
+let handler = async (m, { conn, text }) => {
+  if (!text) throw 'Nyari apa?'
+  let res = await fetch(global.API('https://wall.alphacoders.com/api2.0','/get.php', {
+    auth: '3e7756c85df54b78f934a284c11abe4e',
+    method: 'search',
+    term: text
+  }))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  let img = json.wallpapers[Math.floor(Math.random() * json.wallpapers.length)]
+  await conn.sendButton(m.chat, `Hasil Pencarian\n${text}`, author, await(await fetch(img.url_image)).buffer(), [['Next', m.text]], { quoted: m })
+}
+handler.help = ['wallpaperq <query>']
+handler.tags = ['internet']
+handler.command = /^wall(paper)?q?$/i
+
+module.exports = handler
