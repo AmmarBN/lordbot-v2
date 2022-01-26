f5de54c0b4495a9c0adea89154a8bde665ba82c7
Lord_Ammar <83591869+AmmarBN@users.noreply.github.com>
2022-01-26 14:01:20 +0000

    Add files via upload

---
+++ plugins/script.js
@@ -0,0 +1,23 @@
+let handler  = async (m, { conn, usedPrefix }) => {
+  conn.reply(m.chat, `
+*Lord Botz Menggunakan Script*
+github.com/AmmarB/lordbot-aq-tes
+_*Jangan Lupa Follow Sama Kasi Star Ya kak*_
+
+`.trim(), m)
+}
+handler.help = ['script']
+handler.tags = ['main']
+handler.command = /^(script)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
