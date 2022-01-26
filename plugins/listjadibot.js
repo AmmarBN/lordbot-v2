058996a409e77dcc4e0684c85fbc637d23d5c0df
Lord_Ammar <83591869+AmmarBN@users.noreply.github.com>
2022-01-26 13:35:18 +0000

    Add files via upload

---
+++ plugins/listjadibot.js
@@ -0,0 +1,8 @@
+async function handler(m, { usedPrefix }) {
+  let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
+  m.reply(`🤴 *Owner Botz*\n\nhttps://wa.me/6288229683561\n\n🌟 *Bot Utama* \n\n• https://wa.me/${global.conn.user.jid.split('@')[0]}?text=${usedPrefix}menu (${global.conn.user.name}) \n\n *🤖JadiBot* \n\n` + users.map(v => '• https://wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
+}
+handler.command = handler.help = ['listjadibot']
+handler.tags = ['jadibot']
+
+module.exports = handler
