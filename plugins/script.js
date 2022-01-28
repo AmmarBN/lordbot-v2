61515f58db71a06770f026167c2f45d2867aecd7
Lord_Ammar <83591869+AmmarBN@users.noreply.github.com>
2022-01-28 02:20:00 +0000

    Update script.js

--- plugins/script.js
+++ plugins/script.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn, usedPrefix }) => {
   conn.reply(m.chat, `
 *Lord Botz Menggunakan Script*
-github.com/AmmarB/lordbot-aq-tes
+github.com/AmmarBN/lordbot-aq-tes
 _*Jangan Lupa Follow Sama Kasi Star Ya kak*_
 
 `.trim(), m)
